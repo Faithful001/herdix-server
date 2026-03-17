12be5ce1d1b5c03440baf318c7ecc3506f9d2088
Faithful King Eromosele <83516086+Faithful001@users.noreply.github.com>
2026-03-17 21:06:39 +0100

    Update health check message in AppService

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHealth(): string {
-    return "I'm alive!";
+    return "I'm alive, up and running!";
   }
 }
