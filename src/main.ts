f67ce21587b37792e4eb9dca3e622ec00eb0704e
Faithful King Eromosele <83516086+Faithful001@users.noreply.github.com>
2026-03-24 14:33:40 +0100

    Comment out ResponseInterceptor in bootstrap
    Comment out the ResponseInterceptor usage in bootstrap function.

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
+  // app.useGlobalInterceptors(new ResponseInterceptor());
 
   app.useGlobalGuards(
     new RateLimitGuard(
